845e5bf9640845e96f35a139d017bf5d88d67935
simon <wangzd.fudan@gmail.com>
2016-10-30 15:11:17 +0800

    div the page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,6 +13,7 @@ class GroupsController < ApplicationController
   end
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
   end
   def edit
 
